079f9332a93bddb711c19cb389451e5cd969ea43
Iago Guedelha Nunes <iagognunes@outlook.com>
2024-01-03 16:40:51 -0300

    refactor: revertida mudança na api

--- src/app/api/heroes/route.ts
+++ src/app/api/heroes/route.ts
@@ -1,8 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const apiUrl = process.env.DOMAIN_ORIGIN;
-  const response = await fetch(`${apiUrl}/api/heroes`, { cache: "no-store" });
+  const response = await fetch(`${process.env.API_URL}/api/heroes`);
   const data = await response.json();
 
   return NextResponse.json({ data });
