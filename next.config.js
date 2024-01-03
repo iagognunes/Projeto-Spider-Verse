40d1e1b270f2e68b22bfc479d05b129d11fd3534
Iago Guedelha Nunes <iagognunes@outlook.com>
2024-01-03 16:52:54 -0300

    refactor: Mudança no .env

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     API_URL: "https://658ecdb12871a9866e79d785.mockapi.io",
-    DOMAIN_ORIGIN: "http://localhost:3000",
+    DOMAIN_ORIGIN: "https://projeto-spider-verse.vercel.app/",
   },
 };
 
